ee97608029f61350e264c94c320b517c18db91fc
ninjapj25 <ninjapj25@gmail.com>
2020-09-20 20:11:16 +0800

    Updates

---
+++ precache-manifest.bbbf126ec94e2c1b8530a8b8b8022727.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "085bbb5cb2dcf7a9cd141c2a2a85a4e1",
+    "url": "/WeatherReactApp/index.html"
+  },
+  {
+    "revision": "acce6cbd8337effbfd37",
+    "url": "/WeatherReactApp/static/css/main.b77f591c.chunk.css"
+  },
+  {
+    "revision": "e6ff2e199112b17ae4e3",
+    "url": "/WeatherReactApp/static/js/2.aad77bba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WeatherReactApp/static/js/2.aad77bba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acce6cbd8337effbfd37",
+    "url": "/WeatherReactApp/static/js/main.11e95d34.chunk.js"
+  },
+  {
+    "revision": "707ff971a5d6e6eeb1bd",
+    "url": "/WeatherReactApp/static/js/runtime-main.2d05eaeb.js"
+  },
+  {
+    "revision": "daa04a8bc71ba208007438867c997eb0",
+    "url": "/WeatherReactApp/static/media/Logo.daa04a8b.svg"
+  },
+  {
+    "revision": "058ca36b1fa153ea8a2c192e219867ba",
+    "url": "/WeatherReactApp/static/media/Search.058ca36b.svg"
+  },
+  {
+    "revision": "4bac5cdc50f7313974fb424d064303e8",
+    "url": "/WeatherReactApp/static/media/UPPER DESIGN.4bac5cdc.svg"
+  }
+]);
